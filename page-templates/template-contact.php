144e1c1206e68dabdc2e3db79074c37f55a3bfd9
sijo joseph <inbox.sijojoseph@outlook.com>
2019-02-13 18:12:16 +0530

    contact page

---
+++ page-templates/template-contact.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Template Name: QP - Contact Us
+ * Description: Template for displaying faq page
+ */
+
+get_header();
+?>
+
+
+
+
+  <section class="p-0">
+      <div class="row m-0 p-0">
+        <div class="col-12 m-0 p-0 contact-map">
+          <img class="img-fluid" src="http://localhost/qp/wp-content/uploads/2019/02/map.png" />
+        </div>
+      </div>
+      <div class="contact-bg">
+        <div class="container">
+          <div class="row col-11 contact-info-box mx-auto px-0 mb-3 py-3">
+            <div class="col-12 col-sm-4 col-md-4 col-lg-3 col-xl-3 p-0 m-0 mx-auto">
+  	            <div class="phone-icon">
+  	            	<i class="fas fa-mobile-alt"></i>
+  	            </div>
+  	             <div class="phone-num">
+  	             	<p>HOTLINE</p>
+  	             	<span>+974 0000 0000</span>	
+  	            </div>
+            </div>
+            <div class="col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 p-0 m-0 mx-auto">
+  	            <div class="phone-icon">
+  	            	<i class="far fa-envelope"></i>
+  	            </div>
+  	             <div class="phone-num">
+  	             	<p>EMAIL</p>
+  	             	<span>info@qatarpetrol.qa</span>	
+  	            </div>
+            </div>
+            <div class="col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 p-0 m-0 mx-auto">
+  	            <div class="phone-icon">
+  	            	<i class="fas fa-map-marker-alt"></i>
+  	            </div>
+  	             <div class="phone-num">
+  	             	<p>ADDRESS</p>
+  	             	<span>Doha, Corniche 10000</span>	
+  	            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-sm-12 col-md-12 col-lg-7 col-xl-7 mx-auto p-5">
+            <h1>Help us serve you better by<br>answering this survey</h1>
+            <p>Nunc metus lacus, commodo ac justo eget, mollis lobortis leo. Vestibulum vel vehicula velit?</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          </div>
+          
+        </div>
+      </div>
+  </section>
+
+  
+
+<?php get_footer(); ?>

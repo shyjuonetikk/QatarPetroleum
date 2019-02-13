7a0523e63cc127898be5d8f4cd7e307ba8884bfc
shyjuonetikk <shyju@onetikk.com>
2019-02-13 17:45:44 +0530

    About us page dynamic

--- page-templates/template-about.php
+++ page-templates/template-about.php
@@ -11,7 +11,7 @@ get_header();
         <div id="cover-hgt" class="col-md-9 mx-auto d-flex h-100">
             <div class="row justify-content-left align-self-center">
                 <div id="abt-main-head" class="col-12 justify-content-left align-center">
-                    <h1 id="about-heading" class="text-white">Committed to Excellence</h1>
+                    <h1 id="about-heading" class="text-white"><?php the_field('banner_heading'); ?></h1>
                 </div>
                 <div class="col-12 justify-content-left align-self-top">
                     <ul class="about-sect-links float-left w-100 p-0 col-lg-10 col-md-12">
@@ -35,13 +35,9 @@ get_header();
     <section id="abt-sec">
         <div class="container">
             <div class="col-md-9 mx-auto">
-                <h1 id="abt-desc-title" class="prm-clr"><span class="font-weight-light">AT QATAR PETROLEUM WE ARE COMMITTED TO ONE THING ABOVE ALL: <span class="font-weight-bold">EXCELLENCE.</span></h1>
+                <h1 id="abt-desc-title" class="prm-clr"><span class="font-weight-light"><?php the_field('content_heading'); ?></span></h1>
                 <p id="border-line" class="w-25 sec-bg"></p>
-                <p>Qatar Petroleum (QP) is a state-owned public corporation established by Emiri Decree No. 10 in 1974. It is responsible for all phases of the oil and gas industry in the State of Qatar.</p>
-                <p>The principal activities of QP, its subsidiaries and joint ventures are the exploration, production, local and international sale of crude oil, natural gas and gas liquids, refined products, synthetic fuels, petrochemicals, fuel additives, fertilizers, liquefied natural gas (LNG), steel and aluminium.</p>
-                <p>QP’s strategy of conducting hydrocarbon exploration and development is through Exploration and Production Sharing Agreements (EPSA) and Development and Production Sharing Agreements (DPSA) concluded with major international oil and gas companies.</p>
-                <p>The operations and activities of QP and its affiliates are conducted at various onshore locations, including Doha, Dukhan and the Mesaieed and Ras Laffan Industrial Cities, as well as offshore areas, including Halul Island, offshore production stations, drilling platforms and the North Field.</p>
-                <p>Thriving on a spirit of enterprise, each of our joint ventures is underpinned by transparency, innovation and high standards of quality and service. At Qatar Petroleum, we are committed to one thing above all: Excellence.</p>
+                <?php the_field('content'); ?>
             </div>
         </div>
     </section>
@@ -49,27 +45,28 @@ get_header();
         <div class="container">
             <div id="ceo-border" class="col-12 float-left">
                 <div class="col-xl-6 float-left">
+                    <?php
+
+                        $msgceo = get_field('message_from_ceo');
+                        $ceo_image = $msgceo['ceo_image'];
+                        $ceo_name = $msgceo['ceo_name'];
+                        $position = $msgceo['ministry_position'];
+                        $designation = $msgceo['designation'];
+                        $message = $msgceo['message'];
+
+                    ?>
                     <div class="ceo-image w-100">
-                        <img class="w-100" src="<?php echo get_template_directory_uri(); ?>/img/ceo.png">
+                        <img class="w-100" src="<?php echo $ceo_image; ?>">
                          <div class="ceo-content">
-                            <h4 class="mb-0">Saad Sherida Al-Kaabi</h4>
-                            <p class="text-left mb-0">Minister of State for Energy Affairs</p>
-                            <p class="text-left mb-0">President & CEO</p>
+                            <h4 class="mb-0"><?php echo $ceo_name; ?></h4>
+                            <p class="text-left mb-0"><?php echo $position; ?></p>
+                            <p class="text-left mb-0"><?php echo $designation; ?></p>
                          </div>
                     </div>
                 </div>
                 <div class="col-xl-6 float-left">
                     <div class="ceo-content-desc">
-                        <p>The State of Qatar is moving with great resolve and determination on the road of development and prosperity under the wise leadership of His Highness the Emir, Sheikh Tamim bin Hamad Al Thani. It is managing and utilizing its hydrocarbon resources to build a modern infrastructure and a sustainable knowledge-based economy.</p>
-                        <p>In this effort, Qatar Petroleum as the custodian and developer of the oil and gas resources is at the forefront in helping to achieve Qatar’s vision for sustainable social, economic, human, and environmental development.</p>
-                        <p>Qatar Petroleum is resolved to achieve its strategic objectives to become one of the leading national oil corporations in the world, and to ensure the reinforcement of Qatar’s continued economic growth and expansion through optimized and safe operations, and assets integrity.</p>
-                        <p>The wide spectrum of our oil and gas activities, has but one aim: the future of Qatar and its people. Our purpose is well aligned with the objectives of Qatar’s National Vision 2030, particularly the wise management of exhaustible resources in order to sustain prosperity and “ensure that future generations inherit ample means to meet their aspirations.”</p>
-                        <p>We continue the sustainable development of Qatar’s rich natural gas resources and re-entrench its position as the world’s largest, safest and most reliable LNG producer.</p>
-                        <p>We are redeveloping our maturing oil öelds to reach the longest possible plateau of sustainable production levels to support Qatar’s energy needs and economic development.</p>
-                        <p>We are also creating favorable conditions for the growth and development of a wide array of reöning and downstream industries and products.</p>
-                        <p>Despite the weight and volume of our task, we have never taken our eyes off the need to protect and preserve the environment, and the commitment to developing national human resources. We have also given preeminence to education, training, and career development in our quest to develop a new generation of professionals that will lead this great corporation into the future.</p>
-                        <p>Qatar Petroleum is proud to be a deep-rooted corporate citizen of Qatar, maintaining a dynamic proactive engagement in our society, and a major contributor towards human development at home and abroad through the promotion of education, training, and research.</p>
-                        <p>And as we enter our 5th decade, we are more determined than ever to live up to the legacy of all those who made Qatar Petroleum what it is today, and to the expectations of a whole new generation of Qataris who will take Qatar Petroleum into a successful and prosperous future.</p>
+                        <?php echo $message; ?>
                     </div>
                 </div>
             </div>
@@ -79,8 +76,7 @@ get_header();
         <div class="container">
             <div id="strategy-desc" class="col-12">
                 <h2 class="head-clr font-weight-bold">Strategy and Values</h2>
-                <p>Qatar Petroleum has gone through a remarkable transformation over the past three years as it places itself firmly on the road to realizing a very ambitious vision: “to become one of the best national oil companies in the world, with roots in Qatar and a strong international presence.”</p>
-                <p>In this endeavor, we have developed a set of strategy and values to become our corporate culture and to act as a clear roadmap to higher and wider horizons.</p>
+                <?php the_field('strategy_and_values_intro'); ?>
             </div>
     </section>
     <section id="values-sec" class="float-left w-100 grey-bg pt-0">
@@ -88,34 +84,33 @@ get_header();
             <div class="col-xl-6 float-left">
                 <div class="col-12 value-item-one p-0">
                     <h5 class="values-title prm-clr font-weight-bold">OUR VALUES</h5>
-                    <p>The biggest challenge in our drive to transform Qatar Petroleum is creating and sustaining a new culture, which will shape how we will work together as ‘one team’ going forward.</p>
-                    <p>This is why we have developed a set of values to act as the pillars of our work culture and to give momentum to the capabilities, mindsets, processes, systems and behaviors of our people. The six values are:</p>
-                    <h6>SAFETY</h6>
-                    <p>We care for our people and see safety as a priority for everyone. We are committed to an incident-free, secure, safe and healthy environment for our employees, stakeholders, partners and communities where we operate.</p>
-                    <h6>INTEGRITY</h6>
-                    <p>We place the highest importance on honesty and ethical behavior, always choosing to do the right thing. We value transparency in our dealings with each other and stakeholders. Through this we build strong and successful relationships. We believe results matter, but how we achieve those results also matters.</p>
-                    <h6>RESPECT</h6>
-                    <p>We respect our colleagues, stakeholders, partners and communities and act with care and consideration to build trusting relationships. We value our diversity and each individual’s contribution.</p>
-                    <h6>EXCELLENCE</h6>
-                    <p>We continually improve our processes to achieve greater efficiency, productivity and higher performance. We generate and are open to new ideas. We learn from both our successes and mistakes. Each of us is accountable for the quality, efficiency and effectiveness of our work.</p>
-                    <h6>RESPONSIBILITY</h6>
-                    <p>We are committed to operating in a socially responsible manner, caring for the environment and the communities we impact and our employees. We manage our people and assets responsibly, serving as a catalyst for growth to ensure the prosperity of future generations. We develop everyone for the beneöt of QP and the country with a special emphasis on the capabilities of Qataris.</p>
-                    <h6>COLLABORATION</h6>
-                    <p>We communicate across all areas and levels, sharing information and considering diverse viewpoints to achieve our common goals. We empower others, build ownership, create accountability and enhance cooperation. We recognize and appreciate those who work across teams and groups to achieve QP’s vision and strategy.</p>
+                    <?php 
+
+                        the_field('our_values_intro');
+                        if( have_rows('our_values_list') ):
+                            while ( have_rows('our_values_list') ) : the_row();
+                                echo '<h6>'.get_sub_field('value_name').'</h6>';
+                                the_sub_field('value_description');
+                            endwhile;
+                        else :
+                        endif;
+                    ?>
                 </div>
             </div>
             <div class="col-xl-6 float-left">
                 <div class="col-12 value-item-two p-0">
                     <h5 class="values-title prm-clr font-weight-bold">OUR STRATEGY</h5>
-                    <p>Our strategy is designed to help us deöne our ambition, where we wish to invest, the priorities for improvement and implementation plans as we enter this new era of growth It revolves around six major themes:</p>
-                    <ul id="strat-list">
-                        <li>strengthen QP’s technical capability,</li>
-                        <li>maximize upstream value to State of Qatar,</li>
-                        <li>create a large-scale value-adding international portfolio,</li>
-                        <li>re-inforce Qatar’s LNG & global gas position,</li>
-                        <li>maximize downstream added value, and</li>
-                        <li>promote energy efficiency & optimum energy mix in Qatar.</li>
-                    </ul>
+                    <?php 
+                        the_field('our_strategy_intro');
+                        if( have_rows('our_strategy_list') ):
+                            echo '<ul id="strat-list">';
+                            while ( have_rows('our_strategy_list') ) : the_row();
+                                echo '<li>'.get_sub_field('strategy_list_name').'</li>';
+                            endwhile;
+                        else :
+                            echo '</ul>';
+                        endif;
+                    ?>
                 </div>
             </div>
         </div>
@@ -124,7 +119,7 @@ get_header();
         <div class="container">
             <div class="col-12 float-left">
                 <h2 class="head-clr font-weight-bold">Vision Statement</h2>
-                <p>To become one of the best national oil company in the world, with roots in Qatar and a strong international presence</p>
+                <?php the_field('vision_statement'); ?>
             </div>
         </div>
     </section>

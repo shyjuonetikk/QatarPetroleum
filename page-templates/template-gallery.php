1e25cef9991331f96f48f506860fb78c2a49657f
Jithin Jayaprakash <reacherjlc@gmail.com>
2019-02-15 17:15:11 +0530

    Image gallery

--- page-templates/template-gallery.php
+++ page-templates/template-gallery.php
@@ -15,97 +15,25 @@ get_header();
                 <hr>
             </div>
             <div id="qp-gal-img" class="gallery col-xl-12 float-left">
-<<<<<<< HEAD
-                <div class="col-xl-4 col-md-6 float-left px-2 mb-2">
-                    <a class="pop-up-hover" href="<?php echo get_template_directory_uri(); ?>/img/img-gal-1.png">
-                        <img class="img-fluid qp-gal-img" src="<?php echo get_template_directory_uri(); ?>/img/img-gal-1.png" alt="" title="" />
-                        <div class="img-hover-icon container d-flex w-100 p-0">
-                            <div class="col-12 p-0 w-100 justify-content-center align-self-center text-center">
-                                <i class="fa fa-search" aria-hidden="true"></i>
-                            </div>
-                        </div>
-                    </a>
 
-                    <p class="font-weight-bold">Vivamus mattis mauris sit amet purus elementum ultricies. Donec dictum mi ultricies mauris ultrices</p>
-                </div>
-                <div class="col-xl-4 col-md-6 float-left px-2 mb-2">
-                     <a class="pop-up-hover" href="<?php echo get_template_directory_uri(); ?>/img/img-gal-2.png">
-                        <img class="img-fluid qp-gal-img" src="<?php echo get_template_directory_uri(); ?>/img/img-gal-2.png" alt="" title=""/>
-                        <div class="img-hover-icon container d-flex w-100 p-0">
-                            <div class="col-12 p-0 w-100 justify-content-center align-self-center text-center">
-                                <i class="fa fa-search" aria-hidden="true"></i>
-                            </div>
-                        </div>
-                    </a>
-                    <p class="font-weight-bold">Integer tincidunt, orci a congue auctor, odio risus porttitor sapien</p>
-                </div>
-                <div class="col-xl-4 col-md-6 float-left px-2 mb-2">
-                     <a class="pop-up-hover" href="<?php echo get_template_directory_uri(); ?>/img/img-gal-3.png">
-                        <img class="img-fluid qp-gal-img" src="<?php echo get_template_directory_uri(); ?>/img/img-gal-3.png" alt="" title=""/>
-                        <div class="img-hover-icon container d-flex w-100 p-0">
-                            <div class="col-12 p-0 w-100 justify-content-center align-self-center text-center">
-                                <i class="fa fa-search" aria-hidden="true"></i>
-                            </div>
-                        </div>
-                    </a>
-                    <p class="font-weight-bold">Vestibulum feugiat mi et consequat efficitur. Aenean ex elit, malesuada at efficitur quis, rhoncus sit amet lectus</p>
-                </div>
-                <div class="col-xl-4 col-md-6 float-left px-2 mb-2">
-                     <a class="pop-up-hover" href="<?php echo get_template_directory_uri(); ?>/img/img-gal-4.png">
-                        <img class="img-fluid qp-gal-img" src="<?php echo get_template_directory_uri(); ?>/img/img-gal-4.png" alt="" title=""/>
-                        <div class="img-hover-icon container d-flex w-100 p-0">
-                            <div class="col-12 p-0 w-100 justify-content-center align-self-center text-center">
-                                <i class="fa fa-search" aria-hidden="true"></i>
-                            </div>
-                        </div>
-                    </a>
-                    <p class="font-weight-bold">Vivamus tempor ornare nisi, non dapibus est mollis ac. Vivamus vel vestibulum ligula. Nunc dignissim commodo laoreet</p>
-                </div>
-                <div class="col-xl-4 col-md-6 float-left px-2 mb-2">
-                     <a class="pop-up-hover" href="<?php echo get_template_directory_uri(); ?>/img/img-gal-5.png">
-                        <img class="img-fluid qp-gal-img" src="<?php echo get_template_directory_uri(); ?>/img/img-gal-5.png" alt="" title=""/>
-                        <div class="img-hover-icon container d-flex w-100 p-0">
-                            <div class="col-12 p-0 w-100 justify-content-center align-self-center text-center">
-                                <i class="fa fa-search" aria-hidden="true"></i>
-                            </div>
-                        </div>
-                    </a>
-                    <p class="font-weight-bold">Mauris nec fermentum odio, sit amet scelerisque erat. Aenean congue odio et neque sodales faucibus.</p>
-                </div>
-                <div class="col-xl-4 col-md-6 float-left px-2 mb-2">
-                     <a class="pop-up-hover" href="<?php echo get_template_directory_uri(); ?>/img/img-gal-6.png">
-                        <img class="img-fluid qp-gal-img" src="<?php echo get_template_directory_uri(); ?>/img/img-gal-6.png" alt="" title=""/>
-                        <div class="img-hover-icon container d-flex w-100 p-0">
-                            <div class="col-12 p-0 w-100 justify-content-center align-self-center text-center">
-                                <i class="fa fa-search" aria-hidden="true"></i>
-                            </div>
-                        </div>
-                    </a>
-                    <p class="font-weight-bold">Morbi tincidunt et augue id vehicula</p>
-                </div>
-            </div>
-            <div class="col-12 text-center float-left my-4">
-                <button id="qp-img-more" class="btn btn-success prm-clr sec-bg px-3 py-1 border-0">Show More</button>
-=======
                 <?php
 
-                // check if the repeater field has rows of data
-                if( have_rows('image_gallery') ):
+// check if the repeater field has rows of data
+if (have_rows('image_gallery')):
 
-                    // loop through the rows of data
-                    while ( have_rows('image_gallery') ) : the_row(); ?>
-                    
-                    <div class="col-xl-4 col-md-6 float-left px-2 mb-2">
-                        <a href="<?php echo get_template_directory_uri(); ?>/img/img-gal-1.png">
-                            <img class="img-fluid qp-gal-img" src="<?php the_sub_field('gallery_image'); ?>" alt="<?php the_sub_field('title'); ?>" title="<?php the_sub_field('title'); ?>" />
-                        </a>
-                        <p class="font-weight-bold"><?php the_sub_field('title'); ?></p>
-                    </div>
-                    <?php
-                        endwhile;
-                endif;
-                ?>
->>>>>>> 49f52e56b8d43f34f6f8f374908261744b78c8b5
+	// loop through the rows of data
+	while (have_rows('image_gallery')): the_row();?>
+
+												                    <div class="col-xl-4 col-md-6 float-left px-2 mb-2">
+												                        <a href="<?php echo get_template_directory_uri(); ?>/img/img-gal-1.png">
+												                            <img class="img-fluid qp-gal-img" src="<?php the_sub_field('gallery_image');?>" alt="<?php the_sub_field('title');?>" title="<?php the_sub_field('title');?>" />
+												                        </a>
+												                        <p class="font-weight-bold"><?php the_sub_field('title');?></p>
+												                    </div>
+												                    <?php
+	endwhile;
+endif;
+?>
             </div>
             <!-- <div class="col-12 text-center float-left my-4">
                 <button id="qp-img-more" class="btn btn-success prm-clr sec-bg px-3 py-1 border-0" data-id="<?php echo get_the_id(); ?>">Show More</button>
